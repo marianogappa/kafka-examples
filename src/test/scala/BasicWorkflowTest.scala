abb30d358fe28751e7ac70b372198e3a2868f635
Mariano Gappa <mariano@movio.co>
2015-10-22 23:09:19 +1300

    Waiting for Futures to finish before finishing test. Shutting down consumers. Deleting topics.

--- src/test/scala/BasicWorkflowTest.scala
+++ src/test/scala/BasicWorkflowTest.scala
@@ -3,8 +3,8 @@ import java.util.UUID
 import kafka.consumer.Whitelist
 import kafka.producer.KeyedMessage
 import org.scalatest.{ ShouldMatchers, FunSpec }
-import utils.{ KafkaProducerUtils, KafkaConsumerUtils }
-import scala.concurrent.Future
+import utils.{KafkaAdminUtils, KafkaProducerUtils, KafkaConsumerUtils}
+import scala.concurrent.{Await, Future}
 import scala.concurrent.duration._
 import scala.concurrent.ExecutionContext.Implicits.global
 import kafka.serializer.StringDecoder
@@ -14,33 +14,37 @@ class BasicWorkflowTest extends FunSpec with ShouldMatchers with AwaitCondition 
     it("should produce and consume messages") {
 
       val topic = s"topic-${UUID.randomUUID()}"
+      KafkaAdminUtils.createTopic(topic)
 
       val producer = KafkaProducerUtils.create()
       val consumer = KafkaConsumerUtils.create(consumerTimeoutMs = 5000, autoOffsetReset = "smallest")
 
       val stream = consumer.createMessageStreamsByFilter(new Whitelist(topic), 1, new StringDecoder, new StringDecoder).head
 
-      Future {
+      val producerFuture = Future {
         (1 to 10) foreach { number ⇒
           println(s"Producing Message $number")
           Thread.sleep(10)
           producer.send(new KeyedMessage[Array[Byte], Array[Byte]](topic, s"Message $number".getBytes("UTF-8")))
         }
-      }
+      }.andThen { case _ ⇒ println(s"Finished producing messages") }
 
       var pickedUpTickets = 0
 
-      Future {
+      val consumerFuture = Future {
         println("Consuming")
-        stream foreach { ticket ⇒
-          println(s"Consumed ${ticket.message()}")
+        stream foreach { item ⇒
+          println(s"Consumed ${item.message()}")
           pickedUpTickets += 1
         }
-      }.andThen { case _ ⇒ consumer.shutdown() }
+      }.andThen { case _ ⇒ println(s"Shutting down Consumer"); consumer.shutdown() }
 
       awaitCondition("Didn't consume 10 messages!", 10.seconds) {
         pickedUpTickets shouldBe 10
       }
+
+      KafkaAdminUtils.deleteTopic(topic)
+      List(producerFuture, consumerFuture) foreach (Await.ready(_, 10.second))
     }
   }
 }
