4a185ba6f84bd7a04b8ff41f1496318cc12dedd4
Mariano Gappa <mariano@movio.co>
2015-10-25 23:55:23 +1300

    Refactored Basic Async test so that it closes consumer.

---
+++ src/test/scala/BasicAsyncWorkflowTest.scala
@@ -0,0 +1,53 @@
+import java.util.UUID
+
+import kafka.consumer.Whitelist
+import kafka.producer.KeyedMessage
+import kafka.serializer.StringDecoder
+import org.scalatest.{FunSpec, ShouldMatchers}
+import utils.{AwaitCondition, KafkaAdminUtils, KafkaConsumerUtils, KafkaProducerUtils}
+
+import scala.concurrent.ExecutionContext.Implicits.global
+import scala.concurrent.duration._
+import scala.concurrent.{Await, Future}
+
+class BasicAsyncWorkflowTest extends FunSpec with ShouldMatchers with AwaitCondition {
+  describe("Basic Workflow") {
+    it("should produce and consume messages") {
+      val MessageCount = 10
+      val topic = s"topic-${UUID.randomUUID()}"
+      KafkaAdminUtils.createTopic(topic)
+
+
+      val producer = KafkaProducerUtils.create()
+      val producerFuture = Future {
+        (1 to MessageCount) foreach { number ⇒
+          println(s"Producing Message $number")
+          producer.send(new KeyedMessage[Array[Byte], Array[Byte]](topic, s"Message $number".getBytes("UTF-8")))
+        }
+      }.andThen { case _ ⇒ println(s"Finished producing messages") }
+
+
+      var consumedMessages = 0
+      val consumer = KafkaConsumerUtils.create(consumerTimeoutMs = 5000, autoOffsetReset = "smallest")
+      val stream = consumer.createMessageStreamsByFilter(new Whitelist(topic), 1, new StringDecoder, new StringDecoder).head
+      val consumerFuture = Future {
+        println("Consuming")
+        stream foreach { item ⇒
+          println(s"Consumed ${item.message()}")
+          consumedMessages += 1
+
+          if (consumedMessages >= MessageCount)
+            consumer.shutdown()
+        }
+      }.andThen { case _ ⇒ println(s"Shutting down Consumer"); consumer.shutdown() }
+
+      awaitCondition(s"Didn't consume $MessageCount messages!", 10.seconds) {
+        consumedMessages shouldBe 10
+      }
+
+      producer.close()
+      List(producerFuture, consumerFuture) foreach (Await.ready(_, 10.second))
+      KafkaAdminUtils.deleteTopic(topic)
+    }
+  }
+}
